d03eba9a654db2d69779c88bcddaf215b4c758dc
stark <wsd312@163.com>
2017-10-18 17:48:14 +0800

    router

---
+++ learnvue/src/components/User.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <router-link :to="'/user/' + item.id" v-for="item in userList">{{item.username}} </router-link>
+        <div>
+                <p> 姓名：{{userInfo.username}}</p>
+                <p> 爱好：{{userInfo.hobby}}</p>
+                <p> 性别：{{userInfo.sex}}</p>
+        </div>
+    </div>
+</template> 
+
+<script>
+    let data = [
+        {
+            id:1,
+            tip:'vip',
+            username:'luchang',
+            sex:'男',
+            hobby:'coding'
+        },
+        {
+            id:2,
+            tip:'vip',
+            username:'guomian',
+            sex:'男',
+            hobby:'女'
+        },
+        {
+            id:3,
+            tip:'common',
+            username:'zhangming',
+            sex:'男',
+            hobby:'bug'
+        },
+    ]
+    export default{
+        data(){
+            return{
+                userList:data,
+                userInfo:''
+            }
+        },
+        watch:{
+            $route(){
+                this.getData();
+            }
+        },
+        created(){
+            this.getData();
+        },
+        methods:{
+            getData(){
+                // let id = this.$route;
+                console.log(this.$route);
+                let id = this.$route.params.userId;
+                if(id){
+                    this.userInfo = this.userList.filter((item)=>{
+                        return item.id == id;
+                    })[0]
+                }
+                console.log(this.userInfo);
+                // console.log(this.$route.params.stark);
+            }
+        }
+    }
+</script> 
--- learnvue/src/router/index.js
+++ learnvue/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Work from '@/components/Work'
 import Stark from '@/components/Stark'
+import User from '@/components/User'
 
 Vue.use(Router)
 const UserProfile = { template: `<div> 我是profile 组件 </div>` };
@@ -11,7 +12,7 @@ const Blog = { template: `<div> 我是Blog 组件 </div>` };
 const Info = { template: `<div> 我是Info 组件 </div>` };
 const NotFound = { template: `<div>404 您访问的页面不存在 </div>` };
 const About = { template: `<div> 我是About组件 <router-view> </router-view> </div>` };
-const User = {
+const Users = {
     // template: '<div>User {{ $route.params.id }}</div>'
     template: ' <div class="user"> \
             <h2> User {{ $route.params.id } } </h2> \
@@ -34,14 +35,19 @@ export default new Router({
             component: Work
         },
         {
+            path: '/user/:userId?/:name?',
+            name: 'user',
+            component: User
+        },
+        {
             path: '/stark',
             name: 'stark',
             component: Stark
         },
         // { path: '/user/:id', component: User }
         {
-            path: '/user/:id',
-            component: User,
+            path: '/users/:id',
+            component: Users,
             children: [{
                     // 当 /user/:id/profile 匹配成功，
                     // UserProfile 会被渲染在 User 的 <router-view> 中
@@ -75,7 +81,7 @@ export default new Router({
             path: '*',
             // component: NotFound,
             redirect: (to) => {
-                console.log(to);
+                // console.log(to);
                 if (to.path === '/aaa') {
                     return '/work'
                 } else if (to.path === '/bbb') {
